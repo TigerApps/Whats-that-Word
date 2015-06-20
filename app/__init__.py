bae4ec4b117d2abdb0616d07b87c8e851d49880d
TigerApps <arpitgupta_15@yahoo.co.in>
2015-06-20 19:52:17 +0530

    try_run_one_file

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-
-# intentionally imported at last
-from app import views
--- server.py
+++ server.py
@@ -17,9 +17,43 @@ try:
   httpd.serve_forever()
 except KeyboardInterrupt:
   pass
+
+from flask import Flask
+from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app
-app.run()
-httpd.server_close()
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Index Redirect Page"""
+    image = open('phototest.tif','rb')
+    url="http://api.idolondemand.com/1/api/sync/ocrdocument/v1"
+    apikey="9e791393-6bc4-461f-a0f1-58c3203b64b1"
+
+    def postrequests(function,data={},files={}):
+               data["apikey"]=apikey
+               callurl=url.format(function)
+               r=requests.post(callurl,data=data,files=files)
+               return r.json()
+
+    result=postrequests('ocrdocument',data={'mode':'photo'},files={'file': image})
+    text =  result[u'text_block']
+    text = text[0]
+    text = text[u'text']
+
+    return render_template('index2.html',show=text)
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """redirect wrong urls to inxed"""
+    return redirect(url_for('index'))
+
+
 
 
 
