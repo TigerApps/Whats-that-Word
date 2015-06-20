7e6a8b4a48f97d1b0e2af92db8be8e24fc849f4f
TigerApps <arpitgupta_15@yahoo.co.in>
2015-06-20 19:25:02 +0530

    server

--- server.py
+++ server.py
@@ -11,16 +11,16 @@ PORT = int(os.getenv('VCAP_APP_PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
 
-httpd = Server(("", PORT), Handler)
-try:
-  print("Start serving at port %i" % PORT)
-  httpd.serve_forever()
-except KeyboardInterrupt:
-  pass
-httpd.server_close()
+# httpd = Server(("", PORT), Handler)
+# try:
+#   print("Start serving at port %i" % PORT)
+#   httpd.serve_forever()
+# except KeyboardInterrupt:
+#   pass
+# httpd.server_close()
 
 #!flask/bin/python
 from app import app
-app.run(host='0.0.0.0',port=PORT)
+app.run(host='',port=PORT)
 
 
